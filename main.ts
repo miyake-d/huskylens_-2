dae2b920107abaca6627157595b3b9e57ecd4ac8
miyake-d <hudaikousen.1633@gmail.com>
2024-07-05 22:37:15 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-AI_Camera.initI2c()
-AI_Camera.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
-basic.pause(100)
-control.inBackground(function () {
-    while (true) {
-        AI_Camera.request()
-        if (AI_Camera.isAppear(0, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showIcon(IconNames.Yes)
-        } else {
-            basic.showIcon(IconNames.No)
-        }
-    }
-})
+
